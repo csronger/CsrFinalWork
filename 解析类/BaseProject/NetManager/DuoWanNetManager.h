f906cbc0cc3d13c768f6dc7feebedc987d8a4a59
csronger <tarena@chenshurong.local>
2015-11-03 20:22:52 +0800

    1.在多玩Model中添加DuoWanModel.h来方便引入所有多玩的Model类 2.在NetManager中添加DuoWanRequests进行宏定义多玩的链接请求 3.DuoWanNetManager解析到出装CZ

--- 解析类/BaseProject/NetManager/DuoWanNetManager.h
+++ 解析类/BaseProject/NetManager/DuoWanNetManager.h
@@ -7,7 +7,27 @@
 //
 
 #import "BaseNetManager.h"
+#import "DuoWanModel.h"
+
+//http://lolbox.duowan.com/phone/apiHeroes.php?type=free&v=140&OSType=iOS9.1
+
+typedef NS_ENUM(NSUInteger, HeroType)
+{
+    HeroTypeFree,
+    HeroTypeAll,
+};
+
 
 @interface DuoWanNetManager : BaseNetManager
 
++ (id)getHeroWithType:(HeroType)heroType kCompletionHandle;
+
+//英雄皮肤
++ (id)getHeroSkinsWithHeroName:(NSString *)heroName kCompletionHandle;
+//英雄配音
++ (id)getHeroSoundWithHeroName:(NSString *)heroName kCompletionHandle;
+//英雄视频
++ (id)getHeroVideoWithPage:(NSInteger)page tag:(NSString *)enName kCompletionHandle;
+//英雄出装
++ (id)getHeroCZWithHeroName:(NSString *)enName kCompletionHandle;
 @end
--- 解析类/BaseProject/NetManager/DuoWanNetManager.m
+++ 解析类/BaseProject/NetManager/DuoWanNetManager.m
@@ -7,18 +7,89 @@
 //
 
 #import "DuoWanNetManager.h"
+#import "DuoWanRequestsPath.h"
 
 //很多具有共同点的东西可以统一宏定义，比如
 //凡是自己写的宏定义，都需要用k开头，编码习惯
-#define kOSType   @"OSType":@:"iOS9.1"//当前手机系统版本。需要到info文件中去取
+//#define kOSType   @"OSType":@:"iOS9.1"//当前手机系统版本。需要到info文件中去取
+#define kOSType    @"OSType":[@"iOS" stringByAppendingString:[UIDevice currentDevice].systemVersion]//获取系统当前版本号
+#define kVersionName  @"versionName":@"2.4.0"
+#define kV   @"v":@140
 //把path写到文件头部，使用宏定义形式，方便后期维护
-#define kHeroPath @"http://lolbox.duowan.com/phone/apiHeroes.php"//免费+全部英雄
-#define kHeroSkinPath @"http://box.dwstatic.com/apiHeroSkin.php"//英雄皮肤
-#define kHeroVideoPath @"http://box.dwstatic.com/apiVideoesNormalDuowan.php" //英雄视频
-#define kHeroCZPath @"http://db.duowan.com/lolcz/img/ku11/api/lolcz.php"//英雄出装
 
 @implementation DuoWanNetManager
 
+//免费+全部英雄
++ (id)getHeroWithType:(HeroType)heroType completionHandle:(void (^)(id, NSError *))completionHandle
+{
+    NSMutableDictionary *params = [NSMutableDictionary dictionaryWithDictionary:@{kOSType, kV}];
+    switch (heroType)
+    {
+        case HeroTypeFree: {
+            [params setObject:@"free" forKey:@"type"];
+            break;
+        }
+        case HeroTypeAll: {
+            [params setObject:@"all" forKey:@"type"];
+            break;
+        }
+        default: {
+            NSAssert1(NO, @"%s:type类型不正确",__func__);
+            break;
+        }
+    }
+    
+    //判断是哪种英雄还是所有
+    return [self GET:kHeroPath parameters:params completionHandler:^(id responseObj, NSError *error) {
+        switch (heroType) {
+            case HeroTypeFree: {
+                completionHandle([FreeHeroModel objectWithKeyValues:responseObj], error);
+                break;
+            }
+            case HeroTypeAll: {
+                completionHandle([AllHeroModel objectWithKeyValues:responseObj],error);
+                break;
+            }
+            default: {
+                completionHandle(nil, error);
+                break;
+            }
+        }
+    }];
+}
+
+//皮肤
++ (id)getHeroSkinsWithHeroName:(NSString *)heroName completionHandle:(void (^)(id, NSError *))completionHandle
+{
+    return [self GET:kHeroSkinPath parameters:@{kOSType, kV, kVersionName, @"hero":heroName} completionHandler:^(id responseObj, NSError *error) {
+        completionHandle([HeroSkinModel objectWithKeyValues:responseObj], error);
+    }];
+}
+
+//配音
++ (id)getHeroSoundWithHeroName:(NSString *)heroName completionHandle:(void (^)(id, NSError *))completionHandle
+{
+    return [self GET:kHeroSoundPath parameters:@{kOSType, kV, kVersionName, @"hero":heroName} completionHandler:^(id responseObj, NSError *error) {
+        //json数据是标准数组，不需要解析
+        completionHandle(responseObj, error);
+    }];
+}
+
+//视频
++ (id)getHeroVideoWithPage:(NSInteger)page tag:(NSString *)enName completionHandle:(void (^)(id, NSError *))completionHandle
+{
+    return [self GET:kHeroVideoPath parameters:@{kOSType, kV, @"action":@"l", @"p":@(page), @"src":@"letv", @"tag":enName} completionHandler:^(id responseObj, NSError *error) {
+        completionHandle([HeroVIdeoModel objectWithKeyValues:responseObj], error);
+    }];
+}
+
+//出装
++ (id)getHeroCZWithHeroName:(NSString *)enName completionHandle:(void (^)(id, NSError *))completionHandle
+{
+    return [self GET:kHeroCZPath parameters:@{kOSType, kV, @"championName":enName, @"limit":@7} completionHandler:^(id responseObj, NSError *error) {
+        completionHandle([HeroCZModel objectWithKeyValues:responseObj], error);
+    }];
+}
 @end
 
 
